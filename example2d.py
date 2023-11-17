5d2ca466ad2353806b6cb4b4bd0693e99076aed3
maugeais <sylvain.maugeais@univ-lemans.fr>
2023-11-17 15:13:50 +0100

    cleaning up

--- example2d.py
+++ example2d.py
@@ -36,13 +36,10 @@ if __name__ == "__main__" :
     
     t0 = time.time()
 
-<<<<<<< HEAD
     clf = edsd.edsd(f2, X0=[[-0.5, 0], [0.5, 0], [1, 1]], bounds=bounds, processes=4, classes = 2, verbose = True,
                     N1 = 500, svc=dict(C = 1000, gamma = 1), animate = False)
-=======
-    clf = edsd.edsd(f1, X0=[[-0.5, 0], [0.5, 0], [1, 1]], bounds=bounds, processes=4, classes = 2, verbose = True,
-                    N1 = 500, svc=dict(C = 1000, gamma = 0.01), animate = False)
->>>>>>> master
+    # clf = edsd.edsd(f1, X0=[[-0.5, 0], [0.5, 0], [1, 1]], bounds=bounds, processes=4, classes = 2, verbose = True,
+    #                 N1 = 500, svc=dict(C = 1000, gamma = 0.01), animate = False)
     
     print("Temps de calcul", time.time()-t0)
 
